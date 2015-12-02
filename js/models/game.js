4dc2fdcb321bc9a57e05e236acb7ea740a5c0507
Umer <myemailum14@gmail.com>
2015-12-02 10:56:36 -0500

    done

--- js/models/game.js
+++ js/models/game.js
@@ -19,29 +19,79 @@
 				[2,4,6]
 			]
 		}},
+		initialize : function(){
+			// this.on('X', this.);
+		},
 
 		updateState: function(id) {
-			// your code here
+			var b = this.get('board');
+			b[id] = this.player();
+			this.set('board',b);
 		},
 
 		tie: function() {
-			// your code here
+			return this.get("board").filter(function(e){return e==null; }).length==0
 		},
 		
 		gameOver: function() {
-			// your code here
+			if (this.tie()) {
+				return true;
+			} else if(this.winnerCheck()) {
+				return true;
+			} else {
+				return false;
+			}
 		},
 		
 		player: function() {
-			// your code here
+			return (+this.get('turn')%2)?"O":"X";
 		},
 		
 		winnerCheck: function() {
-			// your code here
+			var players = ["O","X"];
+			var board = this.get('board');
+			var t = this;
+			var found = false;
+			players.forEach(function(ele){
+				var data = [];
+				board.forEach(function(e,i) {
+					if (e==ele) data.push(i);
+				});
+				t.get('winningCombos').forEach(function(combo){
+					if (data.indexOf(combo[0])>-1 && data.indexOf(combo[1])>-1 && data.indexOf(combo[2])>-1) {
+						found = true;	
+					} 
+				});
+				// if (this.checkExistsInArray(this.get))
+			});
+			return found;
 		},
+
+		checkExistsInArray: function(arr, subset){
+			var sublen = subset.length;
+			var len = arr.length - sublen;
+			for (var i = 0; i < len+1; i++) {
+				var found = true;
+				for(var j = 0; j < sublen; j++) {
+					if (arr[i+j] != subset[j]) found = false;
+				}
+				if (found) return true;
+			}
+			return false;
+		},
+
 		
 		doTurn: function(id) {
-			// your code here
+			this.trigger(this.player(), id );
+			this.updateState(id);
+			if (this.gameOver()) {
+				if (this.tie()) {
+					this.trigger('tie', this.player() );
+				} else {
+					this.trigger('win', this.player() );
+				}
+			}
+			this.set('turn', this.get('turn')+1);
 		}
 		
 	})
--- js/views/gameview.js
+++ js/views/gameview.js
@@ -1,40 +1,53 @@
 (function() {
 	app.GameView = Backbone.View.extend({
+
+		initialize: function(){
+			this.game = new app.Game();
+			this.listenTo(this.game, 'X', this.drawX);
+			this.listenTo(this.game, 'O', this.drawO);
+			this.listenTo(this.game, 'win', this.win),
+			this.listenTo(this.game, 'tie', this.tie);
+			this.render();
+			$('#container').html(this.el);
+		},
 		
 		events: {
 			"click": "handleClick"
 		},
 
-		initialize: function() {
-			this.game = new app.Game();
-			// wire up event listeners here
-			// call on render here
-		},
-
 		render: function() {
-			// your code here
+			this.$el.html('<div id="message"></div><table border="1" cellpadding="40"><tbody><tr><td id="0"></td><td id="1"></td><td id="2"></td></tr><tr><td id="3"></td><td id="4"></td><td id="5"></td></tr><tr><td id="6"></td><td id="7"></td><td id="8"></td></tr></tbody></table>')
+			return this;
 		},
 
 		handleClick: function(event) {
 			// your code here
 			// hint: try calling #target on event
+			this.game.doTurn(+event.target.id);
 		},
 
 		drawX: function(id) {
-			// your code here
+			this.$el.find('#'+id).html('X')
 		},
 
 		drawO: function(id) {
-			// your code here
+			this.$el.find('#'+id).html('O')
 		},
 
 		tie: function() {
-			// your code here
+			this.postGame("Tie game");
 		},
 
 		win: function() {
 			// your code here
-		}
+			this.postGame('Player ' + this.game.player()+ ' Wins!');
+		},
+		postGame: function(message) {
+			this.$el.find('td').html("");
+			this.$el.find('#message').html(message);
+			this.game.set(this.game.defaults());
+			this.game.set('turns',0);
+     	}
 
 	});
 })();
